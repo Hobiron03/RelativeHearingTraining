751b3146dce2785bea2c6677be29774e105512e4
HOBIRON <yudainepnep@icloud.com>
2020-10-23 15:38:43 +0900

    Create webpack

---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+/* eslint-disable no-undef */
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/javascripts/main.ts",
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "./javascripts/main.js",
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "ts-loader",
+          },
+        ],
+      },
+      {
+        test: /\.js/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: [["@babel/preset-env", { targets: ">0.25%, not dead" }]],
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(css|sass|scss)/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: "css-loader",
+          },
+          {
+            loader: "sass-loader",
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif)/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "images/[name].[ext]",
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "./stylesheets/main.css",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/templates/index.html",
+      filename: "index.html",
+    }),
+    new CleanWebpackPlugin(),
+  ],
+};
