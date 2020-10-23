bc0a2a3ffcc9bb8728b9c209c6bcd46fd390cbd8
HOBIRON <yudainepnep@icloud.com>
2020-10-23 17:01:08 +0900

    WIP

--- src/javascripts/App.tsx
+++ src/javascripts/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Hello from "./components/Hello/Hello";
+import Piano from "./components/Piano/Piano";
 
 export const App = () => {
   return (
     <div>
       <h1>Hello, React</h1>
-      <Hello></Hello>
+      <Piano />
     </div>
   );
 };
---
+++ src/javascripts/components/Piano/Piano.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Piano = () => {
+  return (
+    <div>
+      <h1>This is Piano</h1>
+    </div>
+  );
+};
+
+export default Piano;
