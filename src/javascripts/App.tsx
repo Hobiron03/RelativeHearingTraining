006194ca34a2ec19624d77d15569afcddb07d8d8
HOBIRON <yudainepnep@icloud.com>
2020-10-24 16:24:24 +0900

    #1 Piano の実装

--- src/javascripts/App.tsx
+++ src/javascripts/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hello from "./components/Hello/Hello";
 import Piano from "./components/Piano/Piano";
+import "../stylesheets/main.scss";
 
 export const App = () => {
   return (
-    <div>
+    <div className="App">
+      <h1>Hello, World</h1>
       <Piano />
     </div>
   );
--- src/javascripts/index.tsx
+++ src/javascripts/index.tsx
@@ -1,10 +1,19 @@
 import * as React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import "../medias/oyasumi.mp3";
+import "../medias/Gb.wav";
+import "../medias/G.wav";
+import "../medias/Ab.wav";
+import "../medias/A.wav";
+import "../medias/Bb.wav";
+import "../medias/B.wav";
 import "../medias/C.wav";
 import "../medias/Cs.wav";
 import "../medias/D.wav";
+import "../medias/Ds.wav";
+import "../medias/E.wav";
+import "../medias/F.wav";
+import "../medias/Fs.wav";
 
 ReactDOM.render(
   <React.StrictMode>
